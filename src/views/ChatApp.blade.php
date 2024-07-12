00cc5c49d1d883c238bd4c9f5c9905c7bb743911
jeevasaas <jeevanantham@saasforest.com>
2024-07-12 11:49:14 +0530

    wdsdasd

--- src/views/ChatApp.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{route('contact')}}" method="POST">
-    @csrf
-    <input type="hidden" name="user_id" value="1">
-    <input type="text" name="message" id="">
-    <input type="submit">
-</form>
